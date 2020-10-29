0750302e5e1ad51f4b947291221d12c8a4608f31
Andrew Hickman <axh@phoebussoftware.com>
2020-10-29 23:26:26 +0000

    Adding walls and new sounds

--- Assets/RocketMovement.cs
+++ Assets/RocketMovement.cs
@@ -7,11 +7,14 @@ public class RocketMovement : MonoBehaviour
 
     Rigidbody rigidbody;
     AudioSource audio;
+    private Vector3 originalPos;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
         audio = GetComponent<AudioSource>();
+        originalPos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
     }
 
     // Update is called once per frame
@@ -24,8 +27,15 @@ public class RocketMovement : MonoBehaviour
     {
         HandleThrust();
         HandleRotation();
+        ResetPosition();
     }
 
+    private void ResetPosition() {
+        if (Input.GetKey(KeyCode.R)) {
+            gameObject.transform.position = originalPos;
+            gameObject.transform.rotation = new Quaternion(0,0,0,1);
+        }
+    }
     private void HandleThrust()
     {
         if (Input.GetKey(KeyCode.Space))
