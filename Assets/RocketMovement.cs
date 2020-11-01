9fa067dc954a88ecbab4dd747d89400bafaf1bae
Andrew Hickman <axh@phoebussoftware.com>
2020-11-01 19:58:02 +0000

    Adding lots of assets, new levels, fps limiter, and particle effects(Which i need to fix)

--- Assets/RocketMovement.cs
+++ Assets/RocketMovement.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RocketMovement : MonoBehaviour
 {
 
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
     Rigidbody rigidbody;
     AudioSource audio;
+
+    enum State {ALIVE, DYING, TRANSENDING}
+    private State state = State.ALIVE;
     private Vector3 originalPos;
 
     // Start is called before the first frame update
@@ -30,21 +36,58 @@ public class RocketMovement : MonoBehaviour
         ResetPosition();
     }
 
-    private void ResetPosition() {
-        if (Input.GetKey(KeyCode.R)) {
+    void OnCollisionEnter(Collision collision)
+    {
+        if(state != State.ALIVE) {
+            return;
+        }
+        if (collision.gameObject.tag.Equals("Friendly"))
+        {
+        }
+        else if (collision.gameObject.tag.Equals("Finish"))
+        {
+            state = State.TRANSENDING;
+            Invoke("LoadNextLevel", 1f);
+        }
+        else
+        {
+            state = State.DYING;
+            Invoke("LoadFirstLevel", 1f);
+        }
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);
+        state = State.ALIVE;
+    }
+
+       private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+        state = State.ALIVE;
+    }
+
+    private void ResetPosition()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
             gameObject.transform.position = originalPos;
-            gameObject.transform.rotation = new Quaternion(0,0,0,1);
+            gameObject.transform.rotation = new Quaternion(0, 0, 0, 1);
         }
     }
     private void HandleThrust()
     {
+        if(state != State.ALIVE) {
+            return;
+        }
         if (Input.GetKey(KeyCode.Space))
         {
             if (!audio.isPlaying)
             {
                 audio.Play();
             }
-            rigidbody.AddRelativeForce(Vector3.up);
+            rigidbody.AddRelativeForce(Vector3.up * mainThrust);
         }
         else
         {
@@ -54,17 +97,19 @@ public class RocketMovement : MonoBehaviour
 
     private void HandleRotation()
     {
+        if(state != State.ALIVE) {
+            return;
+        }
         rigidbody.freezeRotation = true;
 
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(Vector3.back);
-            // rigidbody.AddRelativeForce(Vector3.right);
+            transform.Rotate(Vector3.back * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
-            // rigidbody.AddRelativeForce(Vector3.left);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
 
         rigidbody.freezeRotation = false;
---
+++ Assets/TurnOnParticles.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOnParticles : MonoBehaviour
+{
+    [SerializeField] ParticleSystem particleSystem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space)) {
+            if(!particleSystem.isPlaying) {
+                particleSystem.Play();
+            } 
+            particleSystem.Stop();
+        }
+    }
+}
